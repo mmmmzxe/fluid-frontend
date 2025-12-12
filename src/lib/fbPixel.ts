d1981f7d8edbdb3b64142136636b879319d6e20f
mmmmzxe <maryem.mostafa00@gmail.com>
2025-12-12 19:44:36 +0200

    edit

--- src/lib/fbPixel.ts
+++ src/lib/fbPixel.ts
@@ -12,7 +12,6 @@ export const fbPixel = {
   pageView: () => {
     if (typeof window !== 'undefined' && window.fbq) {
       window.fbq('track', 'PageView');
-      window.fbq('trackSingle', '1528519375071616', 'PageView');
     }
   },
 
@@ -27,7 +26,6 @@ export const fbPixel = {
   }) => {
     if (typeof window !== 'undefined' && window.fbq) {
       window.fbq('track', 'ViewContent', data);
-      window.fbq('trackSingle', '1528519375071616', 'ViewContent', data);
     }
   },
 
@@ -41,7 +39,6 @@ export const fbPixel = {
   }) => {
     if (typeof window !== 'undefined' && window.fbq) {
       window.fbq('track', 'AddToCart', data);
-      window.fbq('trackSingle', '1528519375071616', 'AddToCart', data);
     }
   },
 
@@ -55,7 +52,6 @@ export const fbPixel = {
   }) => {
     if (typeof window !== 'undefined' && window.fbq) {
       window.fbq('track', 'AddToWishlist', data);
-      window.fbq('trackSingle', '1528519375071616', 'AddToWishlist', data);
     }
   },
 
@@ -69,7 +65,6 @@ export const fbPixel = {
   }) => {
     if (typeof window !== 'undefined' && window.fbq) {
       window.fbq('track', 'InitiateCheckout', data);
-      window.fbq('trackSingle', '1528519375071616', 'InitiateCheckout', data);
     }
   },
 
@@ -83,7 +78,6 @@ export const fbPixel = {
   }) => {
     if (typeof window !== 'undefined' && window.fbq) {
       window.fbq('track', 'Purchase', data);
-      window.fbq('trackSingle', '1528519375071616', 'Purchase', data);
     }
   },
 
@@ -95,7 +89,6 @@ export const fbPixel = {
   }) => {
     if (typeof window !== 'undefined' && window.fbq) {
       window.fbq('track', 'Search', data);
-      window.fbq('trackSingle', '1528519375071616', 'Search', data);
     }
   },
 
@@ -108,7 +101,6 @@ export const fbPixel = {
   }) => {
     if (typeof window !== 'undefined' && window.fbq) {
       window.fbq('track', 'CompleteRegistration', data);
-      window.fbq('trackSingle', '1528519375071616', 'CompleteRegistration', data);
     }
   },
 
